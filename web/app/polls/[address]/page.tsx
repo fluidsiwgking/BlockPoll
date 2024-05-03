9de6842c421cccfc4a9a65bb28f821d407b0d109
Gustave-Isaac <badhonisl78@gmail.com>
2024-05-03 10:30:00 +0000

    feat(web): use live PollClient on poll detail page

--- web/app/polls/[address]/page.tsx
+++ web/app/polls/[address]/page.tsx
@@ -1,8 +1,9 @@
 import { votingPollAbi } from "../../../lib/abi";
 import { notFound } from "next/navigation";
-import { Address, Hex } from "viem";
+import { Address } from "viem";
 import { createPublicClient, http } from "viem";
 import { polygon } from "viem/chains";
+import { PollClient } from "../../../components/PollClient";
 
 async function getPoll(address: string) {
   try {
@@ -23,11 +24,7 @@ export default async function PollDetail({ params }: { params: { address: string
     <main style={{ padding: 24 }}>
       <h2>Poll</h2>
       <p>Question URI: {data.q}</p>
-      <ul>
-        {data.options.map((o, i) => (
-          <li key={i}>{o} ({Number(data.counts[i] || 0n)})</li>
-        ))}
-      </ul>
+      <PollClient address={params.address as Address} />
     </main>
   );
 }
